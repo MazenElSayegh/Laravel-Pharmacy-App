b19632dc6869aaaeb58fcedbab5aa876bd0348c1
Ahmed Tahoun <ahmed.tahoun1997@gmail.com>
2023-03-29 17:53:07 +0200

    adding two tables

--- database/migrations/2023_03_29_150315_create_medicines_orders_table.php
+++ database/migrations/2023_03_29_150315_create_medicines_orders_table.php
@@ -4,6 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
+
 return new class extends Migration
 {
     /**
@@ -11,17 +12,21 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('medicines_orders', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
+    Schema::create('medicines_orders', function (Blueprint $table) {
+        $table->bigInteger('medicine_id')->unsigned();
+        $table->bigInteger('order_id')->unsigned();
+        $table->foreign('order_id')->references('id')->on('orders');
+        $table->foreign('medicine_id')->references('id')->on('medicines');
+        // $table->primary([ 'order_id','medicine_id']);
+
+        $table->unsignedInteger('quantity');
+
+        $table->timestamps();
 
 
-            // $table->bigInteger('pharmacy_id')->unsigned();
-            // $table->bigInteger('address_id')->unsigned();
-            // $table->foreign('pharmacy_id')->references('national_id')->on('pharmacies');
-            // $table->foreign('address_id')->references('id')->on('client_addresses');
-        });
-    }
+        
+    });
+}
 
     /**
      * Reverse the migrations.
---
+++ database/migrations/2023_03_29_154352_create_pharmacies_medicines_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pharmacies_medicines', function (Blueprint $table) {
+            $table->bigInteger('medicine_id')->unsigned();
+            $table->bigInteger('pharmacy_id')->unsigned();
+            $table->foreign('pharmacy_id')->references('national_id')->on('pharmacies');
+            $table->foreign('medicine_id')->references('id')->on('medicines');
+           
+            $table->unsignedInteger('quantity');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pharmacies_medicines');
+    }
+};
