6fcf3e3edc45fac12a5e99fd64d04c46ac2abbd1
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-03-29 03:18:06 +0200

    adding doctors table

---
+++ database/migrations/2023_03_29_005802_create_doctors_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('doctors', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('email')->unique();
+            $table->string('password');
+            $table->unsignedBigInteger('national_id')->unique();
+            $table->boolean('is_banned');
+            $table->string('image_path')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    // /**
+    //  * Reverse the migrations.
+    //  */
+    // public function down(): void
+    // {
+    //     Schema::dropIfExists('doctors');
+    // }
+};
