2d3a8808ba8ef649eb618c03fad7a4c8e20baf9d
Muhammad Ashraf <muashrafalii@gmail.com>
2023-04-04 00:39:22 +0200

    Edited Sub

--- resources/views/areas/edit.blade.php
+++ resources/views/areas/edit.blade.php
@@ -1,4 +1,4 @@
-@extends('layouts.app')
+@extends('layouts.sub')
 
 @section('title')
     Edit
--- resources/views/layouts/sub.blade.php
+++ resources/views/layouts/sub.blade.php
@@ -29,7 +29,7 @@
   <!-- summernote -->
   <link rel="stylesheet" href="{{asset('plugins/summernote/summernote-bs4.min.css')}}">
 </head>
-<body class="hold-transition sidebar-mini layout-fixed">
+<body class="sidebar-collapse">
 <div class="wrapper">
 
   <!-- Preloader -->
@@ -41,9 +41,7 @@
   <nav class="main-header navbar navbar-expand navbar-white navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
-      <li class="nav-item">
-        <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
-      </li>
+      
       <li class="nav-item d-none d-sm-inline-block">
         <a href="{{route('home')}}" class="nav-link">Home</a>
       </li>
