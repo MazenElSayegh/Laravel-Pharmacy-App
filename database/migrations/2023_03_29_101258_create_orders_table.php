c4a3c9b2c5b9e585359aeb0dc1dfeed7cde0dad8
Mazen Elsayegh <mazen.elsayegh120298@gmail.com>
2023-04-04 02:50:52 +0200

    making status enum

--- database/migrations/2023_03_29_101258_create_orders_table.php
+++ database/migrations/2023_03_29_101258_create_orders_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
             $table->boolean('is_insured');
-            $table->unsignedInteger('status');
+            $table->enum('status', ['New', 'Processing', 'WaitingForUserConfirmation','Confirmed','Delivered','Canceled']);
             $table->unsignedBigInteger('total_price');
             $table->string('prescription_image')->nullable();
             $table->string('creator_type');
