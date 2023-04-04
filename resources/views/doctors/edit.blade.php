a6aefbe16938726890c91f587604967413b19c01
Ahmed Fahmi <amfahmi.96@gmail.com>
2023-04-04 04:57:25 +0200

    updating doctors update

--- resources/views/doctors/edit.blade.php
+++ resources/views/doctors/edit.blade.php
@@ -39,7 +39,7 @@
         <label for="exampleInputEmail1" class="form-label">Avatar Image</label>
         <input name="avatar_image" type="file" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" >
       </div>
-
+      @if(auth()->user()->hasRole('admin'))
     <div class="mb-3">
       <label for="exampleFormControlTextarea1" class="form-label">Pharmacy name</label>
       <select name="pharmacy_id" class="form-control">
@@ -48,6 +48,7 @@
           @endforeach
       </select>
     </div>
+    @endif
     <button type="submit" class="btn btn-primary">Update</button>
   </form>
  
